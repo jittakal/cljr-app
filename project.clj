77377b1aca02867613ec84b41dfaf5f6a9745c49
Jitendra <jitendra.takalkar@gmail.com>
2011-09-13 20:50:39 +0200

    added dependency for clojure-contrib and swank

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject cljr-app "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "cljr-app"  
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
