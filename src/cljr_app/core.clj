fecad2d9d57fc8bc8399888022646ec5dc146d6f
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2011-09-17 17:22:54 +0200

    added two functions

--- src/cljr_app/core.clj
+++ src/cljr_app/core.clj
@@ -1,3 +1,13 @@
 (ns cljr-app.core)
 
 (println "Hello World")
+
+(defn sayhello
+  [name]
+  (println (str "Hello " name "")))
+
+(defn usingStrEquals
+  [fname lname]
+  (println (if (. fname String/equals lname)
+             (println "First name and Last name should not be same.")
+             (println (str "Your full name is : " fname " " lname)))))
