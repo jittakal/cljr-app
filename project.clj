282ec92f3b4a9ae3b23d2f516963fbfe6fc495fc
Jitendra <jitendra.takalkar@gmail.com>
2011-09-25 20:11:02 +0200

    datastructure added

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljr-app "1.0.0-SNAPSHOT"
   :description "cljr-app"  
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ src/cljr/app/datastructure/numbers.clj
@@ -0,0 +1,14 @@
+;; Clojure uses and supports Java boxed number types derived from
+;; java.lang.Number and its own Ration
+;; a) Integers
+;; b) Ratio (22/7)
+;; c) Float (operation involve double).
+
+(ns cljr.app.datastructure.numbers)
+
+;; computation related functions
+;; + - * / inc dec quot rem min max
+
+;; comparison related functions
+;; == < <= > >= zero? pos? neg?
+
