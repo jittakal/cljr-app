a1b758a33e6adc882be1b8b86e6954f433ff2613
Jitendra <jitendra.takalkar@gmail.com>
2011-09-27 21:55:36 +0200

    sample stack protocol

--- src/cljr/lang/dt/datatypes.clj
+++ src/cljr/lang/dt/datatypes.clj
@@ -9,4 +9,13 @@
            :body (str "GET - Customer : " lname ", " fname)))
   (do-post [this request response]
     (assoc response :status 200
-                    :body (str "POST - Customer : " lname ", " fname))))
+           :body (str "POST - Customer : " lname ", " fname)))
+  Stack
+  (push-into [this coll item]
+    (cons coll item))
+  (pop-off [this coll]
+    (pop coll))
+  (get-first [this coll]
+    (peek coll)))
+
+
--- src/cljr/lang/dt/protocols.clj
+++ src/cljr/lang/dt/protocols.clj
@@ -4,3 +4,11 @@
   "This is sample HTTP protocol"
   (do-get [this request response] "GET")
   (do-post [this request response] "POST"))
+
+(defprotocol Stack
+  "This is sample stack protocol"
+  (push-into [this coll item] "push item in collection")
+  (pop-off [this coll] "pop from the collection")
+  (get-first [this coll] "return the first element"))
+
+
