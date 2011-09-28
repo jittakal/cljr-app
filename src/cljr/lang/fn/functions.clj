7aa3f6f9e5dd551584fc606566170f837172deeb
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2011-09-28 11:35:57 +0200

    local bindings

--- src/cljr/lang/fn/functions.clj
+++ src/cljr/lang/fn/functions.clj
@@ -56,3 +56,18 @@
    (< temp 20) "It's cold"
    (> temp 25) "It's hot"
    :else "It's comfortable"))
+
+;; local bindings
+(defn seconds-to-weeks-v1
+  "Converts seconds to weeks"
+  [seconds]
+  (/ (/ (/ (/ seconds 60) 60) 24) 7))
+
+(defn seconds-to-weeks-v2
+  "Converts seconds to weeks using let form"
+  [seconds]
+  (let [minutes (/ seconds 60)
+        hours (/ minutes 60)
+        days (/ hours 24)
+        weeks (/ days 7)]
+    weeks))
