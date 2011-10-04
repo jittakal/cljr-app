64715c4dadc7a0d9dc56c748aa3f3735edae80c6
Jitendra <jitendra.takalkar@gmail.com>
2011-10-04 22:44:51 +0200

    added images

--- src/cljr/app/todo/view/layout.clj
+++ src/cljr/app/todo/view/layout.clj
@@ -2,6 +2,11 @@
   (:use [hiccup.core :only [html]]
         [hiccup.page-helpers :only [doctype include-css]]))
 
+(defn comment
+  "Wrap the supplied HTML in a comment"
+  [html]
+  (str "<!--" html "-->"))
+
 (defn common [title & body]
   (html
    (doctype :html5)
@@ -10,12 +15,20 @@
     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
     [:title title]
-    (include-css "/css/ie.css"
-                 "/css/print.css"
-                 "/css/screen.css")
+    [:link {:rel "stylesheet" :href "/css/screen.css" :type "test/css" :media "screen projection"}]
+    [:link {:rel "stylesheet" :href "/css/screen.css" :type "test/css" :media "print"}]
+    (comment "[if lt IE 8]><link href=\"/css/ie.css\" media=\"screen,projection\" rel=\"stylesheet\" type=\"text/css\" /><![endif]")
+    [:link {:rel "stylesheet" :href "/css/plugins/fancy-type/screen.css" :type "test/css" :media "screen, projection"}]
     (include-css "http://fonts.googleapis.com/css?family=Sigmar+One&v1")]
    [:body
-    [:div {:id "header"}
+    [:div {:class "container"}
+     [:div {:id "header" :class "span-24 last"}
+      [:h1 {:id "todo"}
+       [:img {:src="images/header.gif" :alt "ToDo" :id "header-image"}]]]
+     [:hr]
+     [:div {:id "subheader" :class "span-24 last"}
+      [:h3 {:class "alt"} "Latest Task"]]
+     [:hr]
      [:h1 {:class "container"} "ToDo"]]
     [:div {:id "content" :class "container"} body]]))
 
