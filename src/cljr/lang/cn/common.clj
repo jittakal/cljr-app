f63a8b0fa442096de9a0adb2cc5b183e81cc07ca
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2011-10-19 16:16:38 +0200

    utility function for html to hiccup - inprogress

--- src/cljr/lang/cn/common.clj
+++ src/cljr/lang/cn/common.clj
@@ -13,3 +13,17 @@
     n
     (+ (fib (- n 1 )) (fib (- n 2)))))
 ;; (dotrace [fib] (fib 3))
+
+
+;; Utility method to convert the html tags into hiccup framework format
+;; Vector and map format
+;; e.g.
+;; 1) I/P => <div>Hello World!</div>
+;;    O/P => [:div "Hello World!"]
+;;
+;; 2) I/P => <div id="main" class="container">
+;;             <div id="submain" class="content">Hello World!</div>
+;;           </div>
+;;    O/P => [:div {:id "main" :class "container"}
+;;             [:div {:id "submain" :class "content"}
+;;               "Hello World!"]]
