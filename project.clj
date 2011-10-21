f5d6487143d413c68b27a2daf4a9e90b5cfcf7c1
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2011-10-21 15:46:58 +0200

    lein ring server to start demo application

--- project.clj
+++ project.clj
@@ -5,9 +5,14 @@
                  [org.clojure.contrib/trace "1.3.0-SNAPSHOT"]
                  [org.clojure.contrib/json "1.3.0-SNAPSHOT"]
                  [ring/ring-jetty-adapter "0.3.11"]
+                 [ring/ring-core "0.3.11"]
+                 [ring/ring-servlet "0.3.11"]
+                 [ring-mock "0.1.1"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
                  [compojure "0.6.4"]
                  [hiccup "0.3.6"]
                  [clj-http "0.2.1"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
+                     [lein-ring "0.4.5"]]
+  :ring {:handler cljr.app.demo.core/demoroutes}
   :repositories {"sonatype-oss-public" "http://oss.sonatype.org/content/groups/public/"})
---
+++ src/cljr/app/demo/core.clj
@@ -0,0 +1,26 @@
+(ns cljr.app.demo.core
+  (:use [compojure.core :only [defroutes GET POST ANY]])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.adapter.jetty :as ring]))
+
+;; Steps to follow
+;; 1. Define Routes using (defroutes)
+;; 2. define acutal routes using (HTTP-VERB url-pattern handler)
+;; handler simply return html code snippet
+;; 3. Start server by registering the earlier define routes
+
+(defroutes demoroutes
+  "All the routes for Demo application"
+  (GET "/getme" [] "<h1>Your Request is GET for /getme modified</h1>")
+  (POST "/postme" [] "<h1>Your Request is POST for /postme</h1>")
+  (ANY "*" [] "<h1>Sorry! unhandled request - ignored</h1>"))
+
+(def demoapp (handler/site demoroutes))
+
+(defn -main []
+  (ring/run-jetty (var demoapp) {:port 8080 :join false}))
+
+
+
+
