0434ec5d082acb543ad01d8915e978bc0d8fc8c6
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2011-12-05 10:51:56 +0100

    commit todo app test cases

--- test/cljr/test/app/todo/model/todoitem.clj
+++ test/cljr/test/app/todo/model/todoitem.clj
@@ -2,8 +2,8 @@
   (:use [cljr.app.todo.model.todoitem])
   (:use [clojure.test]))
 
-(def test-all
-  (is (= 1 (count (all)))))
+;;(def test-all
+;;  (is (= 1 (count (all)))))
 
 ;;(deftest test-create
 ;;  (is (. "Todo Task 1" String/equals (:description (create {:description "Todo;; Task 1" :todolistid 1})))))
--- test/cljr/test/app/todo/model/todolist.clj
+++ test/cljr/test/app/todo/model/todolist.clj
@@ -2,14 +2,14 @@
   (:use [cljr.app.todo.model.todolist])
   (:use [clojure.test]))
 
-(deftest test-all
-  (is (= 2 (count (all)))))
+;;(deftest test-all
+;;  (is (= 2 (count (all)))))
 
 ;;(deftest test-create
 ;;  (is (. "Home" String/equals (:name (create {:name "Home"}))))
 ;;  (is (. "Business" String/equals (:name (create {:name "Business"})))))
 
 
-(deftest test-get-name
-  (is (. "Home" String/equals (:name (first (get-name 1))))))
+;;(deftest test-get-name
+;;  (is (. "Home" String/equals (:name (first (get-name 1))))))
 
