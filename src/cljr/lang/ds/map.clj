f981eafeea8bb54a3bfd5c4732710030c7df3389
Jitendra Takalkar <jitendra.takalkar@gmail.com>
2012-01-10 13:12:32 +0100

    Added documentation

--- src/cljr/lang/ds/map.clj
+++ src/cljr/lang/ds/map.clj
@@ -1,5 +1,19 @@
 (ns ^{:author "Jitendra Takalkar jitendra.takalkar@gmail.com"
       :doc "Sample functions to explore more on map data structure"}
   cljr.lang.ds.map)
+;; A map is a collection thats maps keys and values.
+
+;; Create a new map
+;; The default map is of type hash-map
+(def d-map {:a 1 :b 2 :c 3}) 
+
+;; create simple map using hash-map
+(def h-map (hash-map :a 1 :b 2 :c 3))
+
+;; create sorted map using sorted-map
+;; default sorting will be by ascending order of keys
+(def s-map (sorted-map :c 3 :b 2 :a 1))
+
+;; create sorted map using 
 
 
--- src/cljr/lang/fn/function.clj
+++ src/cljr/lang/fn/function.clj
@@ -7,14 +7,12 @@
   (fn [x y] (* x y)))
 
 ;; define functions with defn
-
 (defn square
   "Squares the provided argument"
   [x]
   (* x x))
 
 ;; functions of multiple arities
-
 (defn square-or-multiply
   "Square a single argument, multiplies two argument"
   ([] 0)
@@ -22,14 +20,12 @@
   ([x y] (* x y)))
 
 ;; functions with variable arguments
-
 (defn add-arg-count
   "Returns the first argument + the number of additional arguments"
   [first & more]
   (+ first (count more)))
 
 ;; shorthand function declaration
-
 (def sq #(* % %))
 
 (def multiply #(* %1 %2))
